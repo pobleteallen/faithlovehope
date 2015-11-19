d8fb209d9ba0252661fce5694044265bdf879fea
pobleteallen <pobleteallen@gmail.com>
2015-11-19 22:27:16 +0800

    erterterterte
    32423

--- js/script.js
+++ js/script.js
@@ -3,37 +3,35 @@ $(document).ready(function(){
 	//after user passed the header div fixed menu will appear
 	$(window).scroll(function(){
 
-		// if($(this).scrollTop() < $('#header')[0].scrollHeight)
-		// {
-		// 	var percentage = Math.round(($(this).scrollTop() / $('#header')[0].scrollHeight)*100);
-		// 	$('#aboutus > *').animate({opacity: percentage/100}, 20);
-		// }
-
-		// if($(this).scrollTop() < $('#contactus')[0].scrollHeight)
+		//page indicator dots
+		if($(this).scrollTop() < $('#aboutus').offset().top-1)
+		{
+			var percentage = Math.round(($(this).scrollTop() / $('#aboutus').offset().top)*100)/100;
+			console.log(percentage);
+			$('#aboutus > *').animate({opacity: percentage}, 20);
+			$('#navigation a').text('○');
+			$('#navigation a[href="#header"]').text('•');
+		}
+		else if($(this).scrollTop() < $('#products').offset().top-1)
+		{
+			$('#navigation a').text('○');
+			$('#navigation a[href="#aboutus"]').text('•');
+		}
+		else if($(this).scrollTop() < $('#contactus').offset().top-1)
+		{
+			$('#navigation a').text('○');
+			$('#navigation a[href="#products"]').text('•');
+		}
+		// else if($(this).scrollTop() < $('#contactus').offset().top-1)
 		// {
 		// 	$('#navigation a').text('○');
 		// 	$('#navigation a[href="#feedbacks"]').text('•');
 		// }
-		// else if($(this).scrollTop() < $('#feedbacks')[0].scrollHeight)
-		// {
-		// 	$('#navigation a').text('○');
-		// 	$('#navigation a[href="#products"]').text('•');
-		// }
-		// else if($(this).scrollTop() < $('#products')[0].scrollHeight)
-		// {
-		// 	$('#navigation a').text('○');
-		// 	$('#navigation a[href="#aboutus"]').text('•');
-		// }
-		// else if($(this).scrollTop() < $('#aboutus')[0].scrollHeight)
-		// {
-		// 	$('#navigation a').text('○');
-		// 	$('#navigation a[href="#header"]').text('•');
-		// }
-		// else
-		// {
-		// 	$('#navigation a').text('○');
-		// 	$('#navigation a[href="#contactus"]').text('•');
-		// }
+		else
+		{
+			$('#navigation a').text('○');
+			$('#navigation a[href="#contactus"]').text('•');
+		}
 	});
 
 
@@ -46,12 +44,10 @@ $(document).ready(function(){
 		$('html,body').animate({scrollTop : $($(this).attr('href')).offset().top}, 1000);
 		return false;
 	});
-
 	//mobile menu
 	$('#mobile-menu').click(function(){
 		$('.ui.sidebar').sidebar({overlay: true, scrollLock: true}).sidebar('show');
 	});
-
 
 	//feedback slide
 	$('.testi:gt(0)').hide();
@@ -66,11 +62,18 @@ $(document).ready(function(){
 
 
 	//products description show when click the image (mobile mode)
-	$('#products .row .column').click(function(){
+	$('#products .row .column').hover(function(){
 		if($(window).width() < 700)
 		{
-			$('.product-description').not($(this).find('.product-description')).slideUp('slow');
-			$(this).find('.product-description').slideToggle('slow');
+			$('.product-description').not($(this).find('.product-description')).slideUp();
+			$(this).find('.product-description').slideToggle();
+		}
+		else
+		{
+			$('.product-description').not($(this).find('.product-description')).slideUp();
+			$(this).find('.segment').slideToggle();
+			// $(this).find('.segment').slideToggle();
+
 		}
 
 	});
